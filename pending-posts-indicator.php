5f6d1450dbf1308bcdc40d603d78bcf440154197
Keith McDuffee <gudlyf@realistek.com>
2014-02-05 21:22:30 -0500

    Committing 1.1.1
    Better support for Wordpress 3.8.1 and positioning of Posts menu item.

--- pending-posts-indicator.php
+++ pending-posts-indicator.php
@@ -1,11 +1,11 @@
 <?php
 /*
 Plugin Name: Pending Posts Indicator
-Plugin URI: http://www.gudlyf.com/2009/01/05/wordpress-plugin-pending-posts-indicator/
+Plugin URI: http://wordpress.org/plugins/pending-posts-indicator/
 Description: Will show a bubble in the left-hand admin menu of the number of posts pending review, if more than zero.
 Author: Keith McDuffee
-Version: 1.0
-Author URI: http://www.gudlyf.com/
+Version: 1.1.1
+Author URI: http://cliqueclack.com/
 */
 
 if(!class_exists('KWM_Pending_Posts_Indicator')) {
@@ -16,14 +16,21 @@ if(!class_exists('KWM_Pending_Posts_Indicator')) {
 
 		function show_pending_number($menu) {
 			
+			$menu_pos = -1;
+			$posts_menu_pos = 3;
 			$num_posts = wp_count_posts( 'post', 'readable' );
 			$status = "pending";
 			$pending_count = 0;
 			if ( !empty($num_posts->$status) )
 				$pending_count = $num_posts->$status;
+			foreach ( $GLOBALS['menu'] as $menuitem ) {
+				$menu_pos++;
+				if( $menuitem[0] == "Posts")
+					$post_menu_pos = $menu_pos;
+			}
 
 			// Use 'plugins' classes for now. May add specific ones to this later.
-			$menu[5] = array( sprintf( __('Posts %s'), "<span class='update-plugins count-$pending_count'><span class='plugin-count'>" . number_format_i18n($pending_count) . "</span></span>" ), 'edit_posts', 'edit.php', '', 'wp-menu-open menu-top', 'menu-posts', 'div' );
+			$menu[$post_menu_pos] = array( sprintf( __('Posts %s'), "<span class='update-plugins count-$pending_count'><span class='plugin-count'>" . number_format_i18n($pending_count) . "</span></span>" ), 'edit_posts', 'edit.php', '', 'open-if-no-js menu-top menu-icon-post', 'menu-posts', 'none' );
 
 			return $menu;
 		}
--- readme.txt
+++ readme.txt
@@ -1,10 +1,10 @@
 === Pending Posts Indicator ===
 Contributors: Keith McDuffee (Gudlyf)
-Donate link: http://astore.amazon.com/cliqueclack-20
+Donate link: http://astore.amazon.com/gudlyf-20
 Tags: admin, editors, administration, enhancement, colaboration, pending
 Requires at least: 2.7.0
-Tested up to: 2.7.0
-Stable tag: 1.0
+Tested up to: 3.8.1
+Stable tag: 1.1
 
 Will show a bubble in the left-hand admin menu of the number of posts pending review, if more than zero.
 
@@ -19,3 +19,11 @@ need of updating.
 
 Activate in plugins admin screen. No options available. Will only display a number next to 'Posts' if
 there are posts pending approval.
+
+== Changelog ==
+
+1.1.1 - Added style changes for newer releases of WP
+
+1.1 - Fixed issue when admin menu is reordered from default, including use of other plugins such as jetpack.
+
+1.0 - Initial release
